bbfb42e7cd733dd2683766817162f8eb7c54de3b
RY-000 <reynardswijaya@gmail.com>
2024-12-30 22:37:52 +0700

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function bookTicket(movieName) {
-    // Redirect to the booking page with the selected movie name as a query parameter
-    window.location.href = `booking.html?movie=${encodeURIComponent(movieName)}`;
-}
